3b2130647059e78ad9ab77126ca651994bd98fb8
oldfartdeveloper <scottnelsonsmith@gmail.com>
2022-01-25 11:21:37 -0800

    Writing 5-char list

--- src/Main.purs
+++ src/Main.purs
@@ -3,9 +3,10 @@ module Main where
 import Prelude
 
 import Data.Array (all, filter, length) as Ar
-import Data.CodePoint.Unicode(isAsciiLower)
-import Data.String.CodePoints(toCodePointArray)
-import Data.String.Utils (length, lines) as Ut
+import Data.CodePoint.Unicode (isAsciiLower)
+import Data.String (joinWith, length)
+import Data.String.CodePoints (toCodePointArray)
+import Data.String.Utils (lines) as Ut
 import Effect (Effect)
 import Effect.Console (log)
 import Node.Encoding (Encoding(ASCII))
@@ -14,11 +15,9 @@ import Node.FS.Sync (readTextFile, writeTextFile)
 main :: Effect Unit
 main = do
   str <- readTextFile ASCII "doc/words.txt"
-  log $ show $ Ar.length (Ar.filter only5CharLowerCase (Ut.lines str))
+  -- log $ show $ Ar.length (Ar.filter only5CharLowerCase (Ut.lines str))
+  writeTextFile ASCII "doc/words5.txt" $ joinWith "\n" (Ar.filter only5CharLowerCase (Ut.lines str))
 
 only5CharLowerCase :: String -> Boolean
 only5CharLowerCase word =
-  (5 == Ut.length word) && (Ar.all isAsciiLower $ toCodePointArray word)
-
--- wordle :: String -> Array String
--- wordle str = []
+  (5 == length word) && (Ar.all isAsciiLower $ toCodePointArray word)
