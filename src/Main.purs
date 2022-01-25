972e99076d64ebd692168ff9b4b898b6f60a0bb4
oldfartdeveloper <scottnelsonsmith@gmail.com>
2022-01-25 11:41:28 -0800

    refactor

--- src/Main.purs
+++ src/Main.purs
@@ -2,22 +2,21 @@ module Main where
 
 import Prelude
 
-import Data.Array (all, filter, length) as Ar
+import Data.Array (all, filter)
 import Data.CodePoint.Unicode (isAsciiLower)
 import Data.String (joinWith, length)
 import Data.String.CodePoints (toCodePointArray)
-import Data.String.Utils (lines) as Ut
+import Data.String.Utils (lines)
 import Effect (Effect)
-import Effect.Console (log)
 import Node.Encoding (Encoding(ASCII))
 import Node.FS.Sync (readTextFile, writeTextFile)
 
 main :: Effect Unit
 main = do
   str <- readTextFile ASCII "doc/words.txt"
-  -- log $ show $ Ar.length (Ar.filter only5CharLowerCase (Ut.lines str))
-  writeTextFile ASCII "doc/words5.txt" $ joinWith "\n" (Ar.filter only5CharLowerCase (Ut.lines str))
+  writeTextFile ASCII "doc/words5.txt" $
+    joinWith "\n" (filter only5CharLowerCase (lines str))
 
 only5CharLowerCase :: String -> Boolean
 only5CharLowerCase word =
-  (5 == length word) && (Ar.all isAsciiLower $ toCodePointArray word)
+  (5 == length word) && (all isAsciiLower $ toCodePointArray word)
