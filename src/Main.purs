91fce989f08c3d4150f00fc2cc7c5b1bd7991969
oldfartdeveloper <scottnelsonsmith@gmail.com>
2022-01-26 12:08:49 -0800

    Refactored to use code points in determining unused letters

--- src/Main.purs
+++ src/Main.purs
@@ -2,10 +2,10 @@ module Main where
 
 import Prelude
 
-import Data.Array (all, filter)
+import Data.Array (all, filter, nub)
 import Data.CodePoint.Unicode (isAsciiLower, isLetter)
 import Data.Foldable (foldl)
-import Data.String (joinWith)
+import Data.String (joinWith, toCodePointArray)
 import Data.String.CodePoints (CodePoint, fromCodePointArray, toCodePointArray)
 import Data.String.CodeUnits (toCharArray)
 import Data.String.Utils (lines)
@@ -31,17 +31,14 @@ attempts =
   ]
 
 -- Letters used but whose position in the answer isn't yet known.
-lettersUsed = "AR"
+lettersUsed :: Array CodePoint
+lettersUsed = toCodePointArray "AR"
 
 -- Calculated letters used, both whose position is known
 -- and whose position are not known.
-letters :: String
+letters :: Array CodePoint
 letters =
-  let
-    cps :: Array CodePoint
-    cps = filter (\c -> isLetter c) $ toCodePointArray lettersInPosition
-  in
-  lettersUsed <> (fromCodePointArray cps)
+  lettersUsed <> (filter (\c -> isLetter c) $ toCodePointArray lettersInPosition)
 
 main :: Effect Unit
 main = do
@@ -50,13 +47,14 @@ main = do
 
 play :: Array String -> String
 play words =
-  letters
+  fromCodePointArray letters
 
 -- What are the letters that are not used in the answer?
-unusedLetters :: String -> Array String -> String
-unusedLetters used attempts =
-  foldl insertUnique "" $ toCharArray $ joinWith "" attempts
+unusedLetters :: Array CodePoint -> Array String -> Array CodePoint
+unusedLetters used atts =
+  filter onlyUnused $ nub $ toCodePointArray $ joinWith "" atts
 
-insertUnique :: String -> Char -> String
-insertUnique acc char =
-  ""
+onlyUnused :: CodePoint -> Boolean
+onlyUnused cp =
+  false
+
