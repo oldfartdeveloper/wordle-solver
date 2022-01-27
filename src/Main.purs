b10ee0e161dc2b64ea867e8d81867c90389ce3c1
oldfartdeveloper <scottnelsonsmith@gmail.com>
2022-01-26 21:16:42 -0800

    Added 'lettersInPositionInternal'

--- src/Main.purs
+++ src/Main.purs
@@ -2,14 +2,16 @@ module Main where
 
 import Prelude
 
-import Data.Array (all, filter, nub)
+import Data.Array (all, filter, mapWithIndex, nub)
+import Data.Char.Utils (toCodePoint)
 import Data.CodePoint.Unicode (isAsciiLower, isLetter)
 import Data.Foldable (any, foldl)
-import Data.String (joinWith, singleton, toCodePointArray)
-import Data.String.CodePoints (CodePoint, fromCodePointArray, toCodePointArray)
+import Data.String (joinWith, singleton)
+import Data.String.CodePoints (CodePoint, codePointAt, codePointFromChar, fromCodePointArray, indexOf, toCodePointArray)
 import Data.String.CodeUnits (contains, toCharArray)
 import Data.String.Pattern (Pattern(Pattern))
 import Data.String.Utils (lines)
+import Data.Tuple (Tuple(Tuple), fst, snd)
 import Effect (Effect)
 import Effect.Class.Console (log)
 import Node.Encoding (Encoding(ASCII))
@@ -20,6 +22,12 @@ unknownPosition = '.'
 
 lettersInPosition :: String
 lettersInPosition = ".U..."
+
+lettersInPositionInternal :: Array (Tuple Int CodePoint)
+lettersInPositionInternal = mapWithIndex convert $ toCharArray lettersInPosition
+  where
+    convert :: Int -> Char -> Tuple Int CodePoint
+    convert offset candidate = Tuple offset (codePointFromChar candidate)
 
 -- Only need 5 attempts
 attempts :: Array String
@@ -36,10 +44,10 @@ lettersUsed :: Array CodePoint
 lettersUsed = toCodePointArray "AR"
 
 -- Calculated letters used, both whose position is known
--- and whose position are not known.
+-- and whose position is not known.
 letters :: Array CodePoint
 letters =
-  lettersUsed <> (filter (\c -> isLetter c) $ toCodePointArray lettersInPosition)
+  lettersUsed <> (filter (\t -> isLetter t) $ map snd lettersInPositionInternal)
 
 main :: Effect Unit
 main = do
@@ -58,4 +66,3 @@ unusedLetters used atts =
 onlyUnused :: CodePoint -> Boolean
 onlyUnused cp =
   not $ contains (Pattern $ singleton cp) $ fromCodePointArray letters
-
